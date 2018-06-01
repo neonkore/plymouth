neonkore/plymouth

commit e4f86e3cbe58eef9a1c7ddff6b41dc85c342f849

renderer: export device name from plugin
Right now the renderer keeps its own copy of the device name, which
may be NULL or out of date after the renderer is opened.

This commit makes sure the device name gets updated to be current.

src/libply-splash-core/ply-renderer.c
@@ -155,6 +155,11 @@ ply_renderer_load_plugin (ply_renderer_t *renderer,
                 return false;
         }
 
+        if (renderer->plugin_interface->get_device_name != NULL) {
+                free (renderer->device_name);
+                renderer->device_name = strdup (renderer->plugin_interface->get_device_name (renderer->backend));
+        }
+
         return true;
 }
 

src/libply-splash-core/ply-renderer-plugin.h
@@ -66,6 +66,8 @@ typedef struct
 
         void (*close_input_source)(ply_renderer_backend_t      *backend,
                                    ply_renderer_input_source_t *input_source);
+
+        const char * (*get_device_name)(ply_renderer_backend_t *backend);
 } ply_renderer_plugin_interface_t;
 
 #endif /* PLY_RENDERER_PLUGIN_H */

src/plugins/renderers/frame-buffer/plugin.c
@@ -365,6 +365,12 @@ open_device (ply_renderer_backend_t *backend)
         return true;
 }
 
+static const char *
+get_device_name (ply_renderer_backend_t *backend)
+{
+        return backend->device_name;
+}
+
 static void
 close_device (ply_renderer_backend_t *backend)
 {
@@ -732,7 +738,8 @@ ply_renderer_backend_get_interface (void)
                 .get_input_source             = get_input_source,
                 .open_input_source            = open_input_source,
                 .set_handler_for_input_source = set_handler_for_input_source,
-                .close_input_source           = close_input_source
+                .close_input_source           = close_input_source,
+                .get_device_name              = get_device_name
         };
 
         return &plugin_interface;

commit bf560986f0fe26f45dc8f8cc0f03ed27e09a4f8d

renderer: nullify device if open fails
Since commit e4f86e3c we query the renderer device straight from
the renderer plugin. Unfortunately, we continue to use that
device name even after the renderer fails to open and the plugin
gets unloaded.

This commit changes the code to go back to using the original
device name in that scenario.

src/libply-splash-core/ply-renderer.c
@@ -51,6 +51,7 @@ struct _ply_renderer
 
         ply_renderer_type_t                    type;
         char                                  *device_name;
+        char                                  *plugin_device_name;
         ply_terminal_t                        *terminal;
 
         uint32_t                               input_source_is_open : 1;
@@ -100,6 +101,9 @@ ply_renderer_free (ply_renderer_t *renderer)
 const char *
 ply_renderer_get_device_name (ply_renderer_t *renderer)
 {
+        if (renderer->plugin_device_name != NULL)
+                return renderer->plugin_device_name;
+
         return renderer->device_name;
 }
 
@@ -156,8 +160,8 @@ ply_renderer_load_plugin (ply_renderer_t *renderer,
         }
 
         if (renderer->plugin_interface->get_device_name != NULL) {
-                free (renderer->device_name);
-                renderer->device_name = strdup (renderer->plugin_interface->get_device_name (renderer->backend));
+                free (renderer->plugin_device_name);
+                renderer->plugin_device_name = strdup (renderer->plugin_interface->get_device_name (renderer->backend));
         }
 
         return true;
@@ -169,6 +173,9 @@ ply_renderer_unload_plugin (ply_renderer_t *renderer)
         assert (renderer != NULL);
         assert (renderer->plugin_interface != NULL);
         assert (renderer->module_handle != NULL);
+
+        free (renderer->plugin_device_name);
+        renderer->plugin_device_name = NULL;
 
         ply_close_module (renderer->module_handle);
         renderer->plugin_interface = NULL;